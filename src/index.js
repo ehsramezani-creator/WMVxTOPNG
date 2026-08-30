69eea49b15373000ceda44a2ce309b5cd78118f4
ehsramezani-creator <ehs.ramezani@gmail.com>
2026-08-30 18:48:05 +0330

    Export model assembler

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { M2LegacyLoader } from './loaders/M2LegacyLoader.js';
 export { SkinLegacyLoader } from './loaders/SkinLegacyLoader.js';
+export { M2SkinResolver } from './loaders/M2SkinResolver.js';
+export { ModelAssembler } from './loaders/ModelAssembler.js';
